e1cc324a057e2ed61ef2bc83fa2b5344cb5d097a
zbwbh <zbw_bh@163.com>
2017-07-19 08:54:57 +0800

    arithmetic program commit

---
+++ arithmetic/src/main/java/com/arithmetic/first/Average.java
@@ -0,0 +1,26 @@
+package com.arithmetic.first;
+
+import com.algs4.stdlib.StdIn;
+import com.algs4.stdlib.StdOut;
+
+/**
+ * 因为那个jar包无法直接使用，结果我是解压之后拷贝到src下面，然后把package改了一下
+ * 另外，其中还少一个jar包，com.sun.j3d，网上搜了一大堆没找到，结果在maven仓库找到了java3d-core的镜像
+ * 一下载结果发现就是这个啊
+ * @author koala
+ * @since 2017年7月18日
+ */
+public class Average {
+
+    public static void main(String[] args) {
+       double sum = 0.0;
+       int cnt = 0;
+       while(!StdIn.isEmpty()){//StdIn.isEmpty方法是找下一个在控制台输入的值
+           //读取一个数并计算累计之和
+           sum+=StdIn.readDouble();
+           cnt++;
+       }
+       double avg = sum/cnt;
+       StdOut.printf("Averate is %.5f\n", avg);
+    }
+}
---
+++ arithmetic/src/main/java/com/arithmetic/first/BaseArithmetic.java
@@ -0,0 +1,59 @@
+package com.arithmetic.first;
+
+/**
+ * 一些最基本的简单算法
+ * 
+ *
+ * @author koala
+ * @since 2017年7月18日
+ */
+public class BaseArithmetic {
+
+    /**
+     * 计算一个整数的绝对值
+     *
+     * @author koala
+     * @since 2017年7月18日
+     */
+    public static int abs(int x){
+        if(x<0)return -x;
+        else return x;
+    }
+    
+    /**
+     * 计算一个浮点数的绝对值
+     *
+     * @author koala
+     * @since 2017年7月18日
+     */
+    public static double abs(double x){
+        if(x<0) return -x;
+        else return x;
+    }
+    
+    /**
+     * 判断一个数是不是素数，素数概念，大于1的自然数
+     * 感觉这个方法有点不对了啊
+     * @author koala
+     * @since 2017年7月18日
+     */
+    public static boolean isPrime(int N) {
+        if(N < 2) return false;//首先如果小于2肯定不是素数了
+        for(int i = 2;i*i<=N;i++)
+            if(N%i==0)return false;
+        return true;
+    }
+    
+    /**
+     * 计算直角三角形的斜边
+     * Math本身是final类，无法被继承，里面的方法还都是本地方法
+     * @author koala
+     * @since 2017年7月18日
+     */
+    public static double xiebian(double a, double b) {
+        return Math.sqrt(a*a+b*b);
+    }
+    
+    public static void main(String[] args) {
+    }
+}

fd3a327190032800b9a7eef43679ed7a73746c58
zbwbh <zbw_bh@163.com>
2017-07-20 16:53:58 +0800

    add queue

---
+++ arithmetic/src/main/java/com/arithmetic/first/Queue.java
@@ -0,0 +1,13 @@
+package com.arithmetic.first;
+
+
+public class Queue {
+
+    public static void queue(int[]a,int head,int second){
+        
+    }
+    
+    public static void main(String[] args) {
+        int a[] = {6,3,1,7,5,8,9,2,4};
+    }
+}

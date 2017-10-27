3df2dc5abd502de7f14c7cedbe4a63e2f038bc21
zbwbh <zbw_bh@163.com>
2017-10-27 15:07:28 +0800

    添加左滑删除功能，在含有iscroll插件的列表当中有效

--- baseframe/src/main/java/com/baseframe/controller/iscrolldelete/IscrollController.java
+++ baseframe/src/main/java/com/baseframe/controller/iscrolldelete/IscrollController.java
@@ -7,8 +7,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/iscroll")
 public class IscrollController {
 
+    /**
+     * 引入Zepto插件，其中的touchWipe给JQ新增了一个函数，用来删除用
+     * 这个还是不叫靠谱的，奈何本人前端基础太差，很多东西都搞不懂
+     * 刚开始让写这个功能的时候还一顿抱怨，其实是自己心太急了，慢慢来就可以，
+     * 前后加在一起这个功能满打满算实际上也就一天而已，不过是间歇性的写而已
+     * 而且，这个功能估计也不重视，所以一直没有催
+     * 
+     * @return
+     *
+     * @author zbw
+     * @since 2017年10月27日
+     */
     @RequestMapping("/delete/one")
     public String iscrollpage() {
         return "wechat/iscroll/iscroll_page";
+    }
+    
+    @RequestMapping("/delete/two")
+    public String iscrollPage() {
+        return "wechat/iscroll/iscroll_page2";
     }
 }
--- baseframe/src/main/webapp/static/js/zepto.touchWipe.js
+++ baseframe/src/main/webapp/static/js/zepto.touchWipe.js
@@ -22,15 +22,17 @@
     var objX = 0; //目标对象位置
 
     $(this).on('touchstart', function(event) {
-      console.log('start..');
+//    console.log('start..');
       var obj = this;
       initX = event.targetTouches[0].pageX;
       initY = event.targetTouches[0].pageY;
-      console.log(initX + ':' + initY);
-      objX = (obj.style.WebkitTransform.replace(/translateX\(/g, "").replace(/px\)/g, "")) * 1;
+//    console.log(initX + ':' + initY);//当前触摸位置，或鼠标点击位置
+	//webkitTransform这个是css3的一个样式，代表着元素变换，其中translate表示平移
+	//有人说在调用该属性的时候需要首字母大写，像这样WebkitTransform，obj.style.WebkitTransform
+      objX = (obj.style.webkitTransform.replace(/translateX\(/g, "").replace(/px\)/g, "")) * 1;
       console.log (objX);
       if (objX == 0) {
-        $(this).on('touchmove', function(event) {
+        $(this).on('touchmove', function(event) {//touchmove事件在touchstart内部
 
           // 判断滑动方向，X轴阻止默认事件，Y轴跳出使用浏览器默认
           if (flagX == 0) {
@@ -42,17 +44,19 @@
             return;
           }
 
+		  //整个下面我都是一点点console出来才有点明白的
+		  //期间我怎么调试都不好使，结果最后在每个li中间加上一个btn才好使
           var obj = this;
           moveX = event.targetTouches[0].pageX;
           X = moveX - initX;  
           if (X >= 0) {
-            obj.style.WebkitTransform = "translateX(" + 0 + "px)";
+            obj.style.webkitTransform = "translateX(" + 0 + "px)";
           } else if (X < 0) {
             var l = Math.abs(X);
-            obj.style.WebkitTransform = "translateX(" + -l + "px)";
+            obj.style.webkitTransform = "translateX(" + -l + "px)";
             if (l > delWidth) {
               l = delWidth;
-              obj.style.WebkitTransform = "translateX(" + -l + "px)";
+              obj.style.webkitTransform = "translateX(" + -l + "px)";
             }
           }
         });
@@ -74,13 +78,13 @@
           X = moveX - initX;
           if (X >= 0) {
             var r = -delWidth + Math.abs(X);
-            obj.style.WebkitTransform = "translateX(" + r + "px)";
+            obj.style.webkitTransform = "translateX(" + r + "px)";
             if (r > 0) {
               r = 0;
-              obj.style.WebkitTransform = "translateX(" + r + "px)";
+              obj.style.webkitTransform = "translateX(" + r + "px)";
             }
           } else { //向左滑动
-            obj.style.WebkitTransform = "translateX(" + -delWidth + "px)";
+            obj.style.webkitTransform = "translateX(" + -delWidth + "px)";
           }
         });
       }
@@ -89,15 +93,15 @@
     //结束时判断，并自动滑动到底或返回
     $(this).on('touchend', function(event) {  
       var obj = this;
-      objX = (obj.style.WebkitTransform.replace(/translateX\(/g, "").replace(/px\)/g, "")) * 1;
+      objX = (obj.style.webkitTransform.replace(/translateX\(/g, "").replace(/px\)/g, "")) * 1;
       if (objX > -delWidth / 2) {
         obj.style.transition = "all 0.2s";
-        obj.style.WebkitTransform = "translateX(" + 0 + "px)";
+        obj.style.webkitTransform = "translateX(" + 0 + "px)";
         obj.style.transition = "all 0";
         objX = 0;
       } else {
         obj.style.transition = "all 0.2s";
-        obj.style.WebkitTransform = "translateX(" + -delWidth + "px)";
+        obj.style.webkitTransform = "translateX(" + -delWidth + "px)";
         obj.style.transition = "all 0";
         objX = -delWidth;
       }

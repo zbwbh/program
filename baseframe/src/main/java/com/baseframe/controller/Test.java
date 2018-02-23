d32954f5b3e9d238079ea5df92f000f32ee469de
zbw <zbw_bh@163.com>
2018-02-23 08:57:19 +0800

    for test

--- baseframe/src/main/java/com/baseframe/controller/Test.java
+++ baseframe/src/main/java/com/baseframe/controller/Test.java
@@ -1,6 +1,8 @@
 package com.baseframe.controller;
 
 
+import java.util.Random;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,5 +22,18 @@ public class Test {
     @RequestMapping("/test2")
     public String test(Model model){
         return "test";
+    }
+    
+    public static void main(String[] args) {
+//        Random ran = new Random();
+//        Integer rand1 = ran.nextInt();
+//        int rand = rand1 & 0x7FFFFFFF;
+//        System.out.println(rand1);
+//        System.out.println(rand);
+        String str = "a";
+        StringBuffer num = new StringBuffer();
+        for (byte b : str.getBytes()) {
+            num.append(Math.abs(b - 96));
+        }
     }
 }
